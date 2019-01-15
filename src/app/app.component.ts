39c155ae0a029f919e1a72793ebbfa5dcc711239
carlosscg <carlosscg@live.com>
2019-01-14 22:08:39 -0500

    directivas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   name = 'is my-first-app';
+  /*
+  showDetails = false;
+  showClickNumber = true;
+  click_number = 0;
+*/
+log = [];
+showSecret = false;
+
+  constructor() {
+
+  }
+
+  /*
+  logClick(){
+    console.log('Click')
+    this.click_number++;
+    this.showClickNumber = true;
+    this.showDetails = !this.showDetails;
+  }
+
+  getColor(){
+    return this.click_number > 4 ? 'blue' : 'white';
+  }
+  */
+  onToogleDetails(){
+   this.showSecret = !this.showSecret;
+   this.log.push(new Date())
+ }
+
 }
