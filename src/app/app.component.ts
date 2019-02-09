48a9d64b5c938771bc7e5268e2494f1f1c642920
carlosscg <carlosscg@live.com>
2019-02-09 09:45:59 -0500

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name = 'is my-first-app';
+  serverElements = [{type: 'server', name: 'TestServer', content:'Just a test'}]
+
+  onServerAdded(serverData:{serverName: string, serverContent: string }) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBluePrintAdded(bluePrintData:{serverName: string, serverContent: string }) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: bluePrintData.serverName,
+      content: bluePrintData.serverContent
+    });
+  }
 }
